3dda1fa83a450744eb4dc1dd754670ecab8b924f
MrYacha <YachaGorobtsov@gmail.com>
2020-04-09 16:55:34 +0300

    Add missing copyrights header

--- obot/modules/inline.py
+++ obot/modules/inline.py
@@ -1,3 +1,14 @@
+# Copyright (C) 2019 The Raphielscape Company LLC.
+# Copyright (C) 2018 - 2020 MrYacha
+# Copyright (C) 2018 - 2020 Sophie
+# Copyright (C) 2020 oBOT
+#
+# This file is part of oBOT.
+#
+# oBOT is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
 import hashlib
 
 from aiogram.types import InlineQuery, InputTextMessageContent, InlineQueryResultArticle
